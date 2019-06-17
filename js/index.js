fce067ec9b9b0f11a1d7556e09091edc74fac9ab
Pradeep Kshirsagar <pradeep99909@gmail.com>
2019-06-17 20:54:18 +0530

    added ml5 code

--- js/index.js
+++ js/index.js
@@ -1,19 +1,24 @@
 window.onload=image;
 
-function image(){
+var image=()=>{
 var classifier = ml5.imageClassifier('MobileNet',function(){
     console.log('Model is loaded')
 });
-var img = document.getElementById('image')
+var img = document.getElementById('image');
 
 
 
-classifier.predict(img, function(err, results) {
-    return(results[0].className)
+classifier.classify(img, function(err, results) {
+    if(err){
+        document.getElementById('classify').innerHTML=err
+    }
+    document.getElementById('classify').innerHTML="<p>"+results[0].label+"</p>"
 });
 
+};
+
+
+var chan=(value)=>{
+    document.getElementById('image').src=value;
+    image()
 }
-
-
-
-
